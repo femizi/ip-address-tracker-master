2e4bfc7cde84486b51bd1b3ed7688f1b382ea59e
femizi <obito.femi@gmail.com>
2022-01-25 09:49:16 +0100

    breaking change maybe?

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ L.tileLayer(
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 13,
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
